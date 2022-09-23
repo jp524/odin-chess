54a4dab536a35b1a5f1a37b37ef1d68cc0bc46f5
JP <85654561+jp524@users.noreply.github.com>
2022-09-23 09:06:13 -0400

    Build #king and associated methods in Moves

---
+++ lib/moves.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: false
+
+# Generates the available moves on the board for a chess piece
+class Moves
+  def initialize
+    @columns = %w[a b c d e f g h]
+  end
+
+  def king(start)
+    # start will be a valid square, such as 'a3'
+    coord = square_to_coord(start)
+    y, x = coord
+    moves = [[y + 1, x - 1],
+             [y + 1, x],
+             [y + 1, x + 1],
+             [y, x - 1],
+             [y, x + 1],
+             [y - 1, x - 1],
+             [y - 1, x],
+             [y - 1, x + 1]]
+    filter_sort_moves(moves)
+    moves.map { |move| coord_to_square(move) }
+  end
+
+  def filter_sort_moves(moves)
+    moves.filter! { |move| (0..7).include?(move[0]) && (0..7).include?(move[1]) }
+    moves.sort_by! { |move| move }
+    moves
+  end
+
+  def square_to_coord(square)
+    column = square[0]
+    row = square[1].to_i
+    row_index = row - 1
+    column_index = @columns.index(column)
+
+    [row_index, column_index]
+  end
+
+  def coord_to_square(coord)
+    row_index = coord[0]
+    column_index = coord[1]
+    row = row_index + 1
+    column = @columns[column_index]
+
+    "#{column}#{row}"
+  end
+end
---
+++ spec/spec_moves.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: false
+
+require_relative '../lib/moves'
+
+describe Moves do
+  subject(:moves) {described_class.new }
+
+  describe '#square_to_coord' do
+    context 'when square "a1" is given' do
+      it 'returns coordinates [0, 0]' do
+        result = moves.square_to_coord('a1')
+        expect(result).to eq([0, 0])
+      end
+    end
+  end
+
+  describe '#coord_to_square' do
+    context 'when coordinates [8, 8] are given' do
+      it 'returns square "h8"' do
+        result = moves.coord_to_square([7, 7])
+        expect(result).to eq('h8')
+      end
+    end
+  end
+
+  describe '#king' do
+    context 'when starting square "f5" is given' do
+      it 'returns an array containing 8 possible finish squares' do
+        result = moves.king('f5')
+        expect(result.length).to eq(8)
+      end
+    end
+
+    context 'when starting square "a1" is given' do
+      it 'returns an array containing 3 possible finish squares' do
+        result = moves.king('a1')
+        expect(result.length).to eq(3)
+      end
+    end
+  end
+end
