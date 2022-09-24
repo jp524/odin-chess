ff4248d4fff141c47c311743935c49ceecd9b2af
JP <85654561+jp524@users.noreply.github.com>
2022-09-24 10:44:05 -0400

    Delete Player class

--- lib/game.rb
+++ lib/game.rb
@@ -1,18 +1,14 @@
 # frozen_string_literal: false
 
 require_relative './board'
-require_relative './player'
 
 # Contains instances of Board and Player to run the game
 class Game
   def initialize
     @board = Board.new
-    @player_w = Player.new('W')
-    @player_b = Player.new('B')
   end
 
   def display
-    @board.send_to_board(@player_w, @player_b)
     @board.display
   end
 end
